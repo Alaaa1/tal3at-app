970b180545303a37f98f74e48c60304b5d1d7971
ahmed-922 <ahmed12c31@gmail.com>
2024-07-05 17:06:34 +0300

    Added new features, comments section Login now will direct you to login page There is Home, search, upload, likes and users buttons will direct you to another pages Tal3at text will redircet you to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import TopNavigation from './components/TopNavigation';
 import HomePage from './pages/homePage/HomePage';
+import Login from './pages/Login';
+import Register from './pages/register'; // Import the Register component
+
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TopNavigation />
-      <HomePage />
+      <Router>
+        <TopNavigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </QueryClientProvider>
   );
 }
